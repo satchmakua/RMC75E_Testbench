29e5496fe01feb483106787e58b5a61dd184ac03
Satchel Hamilton <47583278+satchmakua@users.noreply.github.com>
2023-06-30 17:03:28 -0700

    -
    -

--- TEST/tb_SSITop.vhd
+++ TEST/tb_SSITop.vhd
@@ -26,7 +26,28 @@ architecture tb of tb_SSITop is
     signal H1_CLKWR_tb : std_logic := '0';
     signal intData_tb : std_logic_vector(31 downto 0) := (others => '0');
     signal ssiDataOut_tb : std_logic_vector(31 downto 0);
-    signal SlowEnable_tb : std_logic := '0'; 
+    signal SlowEnable_tb : std_logic := '0';
+
+    -- Define a procedure to encapsulate the code
+    procedure tick_sequence(signal SSI_DATA : out std_logic; signal SynchedTick : out std_logic; signal PositionRead : out std_logic; signal StatusRead : out std_logic) is
+    begin
+        SSI_DATA <= '1'; 
+        wait for 8 us;
+        SSI_DATA <= '0';
+        wait for 22 us;
+        
+        SynchedTick <= '1'; 
+        wait for SysClk_period;
+        SynchedTick <= '0';
+        
+        PositionRead <= '1';
+        wait for 10 us;
+        PositionRead <= '0';
+        
+        StatusRead <= '1';
+        wait for 10 us;
+        StatusRead <= '0';
+    end procedure tick_sequence;
 
 begin
 
@@ -85,53 +106,36 @@ begin
 
     init_gen: process
     begin
+		
         wait for SysClk_period; -- wait for one 30 MHz clock cycle
+				
+				SynchedTick_tb <= '1'; -- first tick syncs system
+        wait for SysClk_period;
+        SynchedTick_tb <= '0';
+        wait for SysClk_period;
+				
         ParamWrite1_tb <= '1'; -- start of system configuration
         intData_tb(31 downto 0) <= "00000000000000000000100000000110";
         wait for SysClk_period;
         ParamWrite1_tb <= '0';
-        wait for 2 us;
-        
-				
-				SynchedTick_tb <= '1'; -- first tick
         wait for SysClk_period;
-        SynchedTick_tb <= '0';
-        wait for 1 us - 4 * H1_CLK_PERIOD;
-				
 				
         ParamWrite2_tb <= '1';
-        intData_tb(31 downto 0) <= "00000000000000100000000000000000";
+        intData_tb(31 downto 0) <= "00000000000000100000000000000010";
         wait for SysClk_period;
         ParamWrite2_tb <= '0';
         wait for SysClk_period;
+
+        -- Loop that calls the defined procedure
+        for i in 1 to 5 loop
+            tick_sequence(SSI_DATA_tb, SynchedTick_tb, PositionRead_tb, StatusRead_tb); 
+        end loop;
         
-        
-        
-        SSI_DATA_tb <= '1'; -- start of 8us pulse on SSI_DATA_tb
-        wait for 8 us;
-        wait for H1_CLK_PERIOD;
-        SSI_DATA_tb <= '0'; -- end of 8us pulse on SSI_DATA_tb
-        wait for 30 us - 1 us - 8 us;
-				
-				-- Begin SSI Read Cycle
-        wait for 1 us;
-        SynchedTick_tb <= '1'; -- second tick
-        wait for H1_CLK_PERIOD;
-				SynchedTick_tb <= '0';
-				
-        PositionRead_tb <= '1'; -- PositionRead active after the second SynchedTick
         wait for 10 us;
-        PositionRead_tb <= '0';
-				
-        StatusRead_tb <= '1';
-        wait for 10 us;
-				StatusRead_tb <= '0';
-				
-        wait for 10 us;
-				
+
         -- Run for the desired number of cycles
         for i in 1 to num_cycles loop
-            wait for H1_CLK_PERIOD;
+            wait for H1_CLK_period;
         end loop;
 
         -- Stop the simulation
---
+++ add_signals.tcl
@@ -0,0 +1,21 @@
+# Open the waveform viewer
+add wave -position insertpoint \
+    -radix decimal \
+    /tb_SSITop/uut/ClkOn \
+    /tb_SSITop/uut/ShiftOn \
+    /tb_SSITop/uut/TurnShiftOff \
+    /tb_SSITop/uut/PreTurnShiftOff \
+    /tb_SSITop/uut/intSSI_CLK \
+    /tb_SSITop/uut/ToggleEn \
+    /tb_SSITop/uut/Enable \
+    /tb_SSITop/uut/SequenceOn \
+    /tb_SSITop/uut/CycleCounter \
+    /tb_SSITop/uut/HalfPeriod \
+    /tb_SSITop/uut/CycleCountMatch \
+    /tb_SSITop/uut/ShiftCounter \
+    /tb_SSITop/uut/StartRead \
+    /tb_SSITop/uut/CheckDataHi \
+    /tb_SSITop/uut/CheckDataLo \
+    /tb_SSITop/uut/CheckDataDelay
+
+
